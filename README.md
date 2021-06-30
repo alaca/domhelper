d2bc14e96aa8e055773fdef60a7ab4fca22de463
alaca <ante.laca@gmail.com>
2021-06-30 21:54:51 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,284 @@
-# domhelper
+# DOMhelper
+
+A small helper utility that provides basic methods for DOM manipulation. API inspired by jQuery.
+
+## API
+
+* [.on(event, [selector], function)](#onevent-selector-function)
+* [.each(function)](#eachfunction)
+* [.addClass(className)](#addclassclassname)
+* [.removeClass(className)](#removeclassclassname)
+* [.toggleClass(className)](#toggleclassclassname)
+* [.hasClass(className)](#toggleclassclassname)
+* [.closest(selector)](#closestselector)
+* [.parent()](#parent)
+* [.first()](#first)
+* [.last()](#last)
+* [.eq(index)](#eqindex)
+* [.remove()](#remove)
+* [.show()](#show)
+* [.hide()](#hide)
+* [.toggle()](#toggle)
+* [.attr(name, [value])](#attrname-value)
+* [.removeAttr(name)](#removeattrname)
+* [.html([html])](#htmlhtml)
+* [.text([text])](#texttext)
+* [.data(key, [value])](#datakey-value)
+* [.append(node|string)](#appendnodestring)
+* [.prepend(node|string)](#prependnodestring)
+* [.css(styles)](#cssstyles)
+
+
+#### .on(event, [selector], function)
+
+Attach an event handler function to the selected elements.
+
+```js
+$( '#elementId' ).on( 'click', function() {
+  console.log( $( this ).html() );
+});
+```
+
+Event delegation
+```js
+$( document ).on( 'click', '#elementId', function() {
+  console.log( $( this ).html() );
+} );
+```
+
+#### .each(function)
+
+Iterate over a NodeList object, executing a function for each matched Element. 
+
+```js
+$( 'li' ).each( function( element, index ) {
+  console.log( $( element ).html() );
+} );
+```
+
+#### .addClass(className)
+
+Adds the specified class to element.
+
+```js
+$( '#elementId' ).addClass( 'test-class' );
+```
+
+Adding multiple classes
+
+```js
+$( '#elementId' ).addClass( 'one' ).addClass( 'two' );
+```
+
+#### .removeClass(className)
+
+Removes the specified class from element.
+
+```js
+$( '#elementId' ).removeClass( 'test-class' );
+```
+
+#### .toggleClass(className)
+
+Add or remove class from element, depending on the class's presence.
+
+```js
+$( '#elementId' ).toggleClass( 'test-class' );
+```
+
+#### .hasClass(className)
+
+Determine whether any of the matched elements are assigned the given class.
+
+```js
+if ( $( '#elementId' ).hasClass( 'test-class' ) ) {
+	console.log( 'yep' );
+}
+```
+
+#### .closest(selector)
+
+Get the first element that matches the selector by testing the element itself and traversing up through its ancestors in the DOM tree.
+
+```js
+const closestSection = $( '#elementId' ).closest( '.section' );
+console.log( closestSection.html() );
+```
+
+#### .parent()
+
+Get the parent element.
+
+```js
+const parentElement = $( '#elementId' ).parent();
+console.log( parentElement.html() );
+```
+
+#### .first()
+
+Reduce the set of matched elements to the first in the set.
+
+```js
+$( '#navigation li' ).first().addClass( 'first' );
+```
+
+#### .last()
+
+Reduce the set of matched elements to the final one in the set.
+
+```js
+$( '#navigation li' ).last().addClass( 'last' );
+```
+
+#### .eq(index)
+
+Reduce the set of matched elements to the one at the specified index.
+
+```js
+$( '#navigation li' ).eq(2).addClass( 'third' );
+```
+
+#### .remove()
+
+Remove the set of matched elements from the DOM.
+
+```js
+$( '.section' ).remove();
+```
+
+#### .show()
+
+Display the matched elements.
+
+```js
+$( '.section' ).show();
+```
+
+#### .hide()
+
+Hide the matched elements.
+
+```js
+$( '.section' ).hide();
+```
+
+#### .toggle()
+
+Display or hide the matched elements.
+
+```js
+$( '.section' ).toggle();
+```
+
+#### .attr(name, [value])
+
+Get the value of an attribute for the first element in the set of matched elements or set attribute for every matched element.
+
+```js
+$( 'a' ).attr( 'href', 'https://github.com/alaca/domhelper');
+```
+
+Get attribute
+
+```js
+const href = $( 'a.latest' ).attr( 'href' );
+console.log( href );
+```
+
+#### .removeAttr(name)
+
+Remove an attribute from each element in the set of matched elements.
+
+```js
+$( 'a' ).removeAttr( 'href' );
+```
+
+#### .html([html])
+
+Get the HTML contents of the first element in the set of matched elements.
+
+```js
+$( '.section' ).html( '<h1>Section content</h1>' );
+```
+
+Get html
+
+```js
+const htmlContent = $( '.section' ).html();
+console.log( htmlContent );
+```
+
+#### .text([text])
+
+Get the text contents of the first element in the set of matched elements.
+
+```js
+$( '.section' ).text( 'Section content' );
+```
+
+Get text
+
+```js
+const textContent = $( '.section' ).text();
+console.log( textContent );
+```
+
+#### .data(key, [value])
+
+Store arbitrary data associated with the specified element or return the value that was set.
+
+```js
+$( '.section' ).data( 'info', 'Additional info' );
+```
+
+Get data attribute value
+
+```js
+const info = $( '.section' ).data( 'info' );
+console.log( info );
+```
+
+#### .append(node|string)
+
+Insert content to the end of each element in the set of matched elements.
+
+```js
+$( '.section' ).append( ' this is appended text' );
+```
+
+Append node
+
+```js
+const element = document.createElement( 'div' );
+element.innerText = 'Appended element content';
+
+$( '.section' ).append( element );
+```
+
+#### .prepend(node|string)
+
+Insert content to the beginning of each element in the set of matched elements.
+
+```js
+$( '.section' ).prepend( ' this is prepended text' );
+```
+
+Append node
+
+```js
+const element = document.createElement( 'div' );
+element.innerText = 'Prepended element content';
+
+$( '.section' ).prepend( element );
+```
+
+#### .css(styles)
+
+Set CSS styles of each element in the set of matched elements.  
+
+```js
+$( '.section' ).css( { 
+  color: 'red', 
+  fontWeight: 'bold'
+} );
+```
--- src/domhelper.js
+++ src/domhelper.js
@@ -1,237 +1,335 @@
 window.DOMHelper = ( function() {
-    'use strict';
+	'use strict';
 
-    const instances = [];
+	/**
+	 *
+	 * @type {<Object>[]}
+	 */
+	const instances = [];
 
 	class $ {
 
-        elements = [];
+		/**
+		 *
+		 * @type {Object[]|NodeListOf<Object>}
+		 */
+		elements;
 
-        /**
-         * Constructor
-         *
-         * @param {string|object} selector
-         */
-        constructor(selector) {
-            if (typeof selector === 'string') {
-                this.elements = document.querySelectorAll(selector);
-            } else {
-                this.elements = [selector];
-            }
-        }
-        
-        /**
-         * Iterate elements
-         * @param {Function} callback 
-         */
-        each(callback) {
-            if (typeof callback === 'function') {
-                this.elements.forEach((element, i) => {
-                    callback(element, i);
-                });
-            }
-        }
+		/**
+		 * Constructor
+		 *
+		 * @param {string|object} selector
+		 */
+		constructor( selector ) {
+			if ( typeof selector === 'string' ) {
+				this.elements = document.querySelectorAll( selector );
+			} else {
+				this.elements = [ selector ];
+			}
+		}
 
-        /**
-         * Attach event
-         * 
-         * @param {string} event 
-         * @param {string|Function} selector 
-         * @param {Function} callback 
-         */
-        on(event, selector, callback) {
-            if (typeof selector === 'function') {
-                this.each( element => {
-                    element.addEventListener(event, function(e) {
-                        const bindElement = selector.bind(e.target)
-                        bindElement(e);
-                    }, false)
-                });
-                return this;
-            }
-            // Event delegation
-            if (typeof selector === 'string' && typeof callback === 'function') {
-                this.each(element => {
-                    element.addEventListener(event, function(e) {
-                        if (e.target.matches(selector)) {
-                            const bindElement = callback.bind(e.target)
-                            bindElement(e);
-                        }
-                    }, false);
-                });
-            }
-            return this;
-        }
+		/**
+		 * Iterate elements
+		 * @param {Function} callback
+		 */
+		each( callback ) {
+			this.elements.forEach( ( element, i ) => {
+				callback( element, i );
+			} );
+		}
 
-        /**
-         * Add class to elements
-         * @param {string} className 
-         */
-        addClass(className) {
-            this.each(element => {
-                element.classList.add(className);
-            });
-            return this;
-        }
+		/**
+		 * Attach event
+		 *
+		 * @param {string} event
+		 * @param {string|Function} selector
+		 * @param {Function} [callback]
+		 * @return {$}
+		 */
+		on( event, selector, callback ) {
+			if ( typeof selector === 'function' ) {
+				this.each( element => {
+					element.addEventListener( event, function( e ) {
+						const bindElement = selector.bind( e.target );
+						bindElement( e );
+					}, false );
+				} );
+				return this;
+			}
+			// Event delegation
+			if ( typeof selector === 'string' && typeof callback === 'function' ) {
+				this.each( element => {
+					element.addEventListener( event, function( e ) {
+						if ( e.target.matches( selector ) ) {
+							const bindElement = callback.bind( e.target );
+							bindElement( e );
+						}
+					}, false );
+				} );
+			}
+			return this;
+		}
 
-        /**
-         * Remove class from elements
-         * @param {string} className 
-         */
-        removeClass(className) {
-            this.each(element => {
-                element.classList.remove(className);
-            });
-            return this;
-        }
+		/**
+		 * Add class to elements
+		 * @param {string} className
+		 * @return {$}
+		 */
+		addClass( className ) {
+			this.each( element => {
+				element.classList.add( className );
+			} );
+			return this;
+		}
 
-        /**
-         * 
-         * @param {string} className 
-         */
-        toggleClass(className) {
-            this.each(element => {
-                element.classList.toggle(className);
-            });
-            return this;
-        }
+		/**
+		 * Remove class from elements
+		 * @param {string} className
+		 * @return {$}
+		 */
+		removeClass( className ) {
+			this.each( element => {
+				element.classList.remove( className );
+			} );
+			return this;
+		}
 
-        /**
-         * Show elements
-         */
-        show() {
-            this.each(element => {
-                element.style.display = '';
-            });
-            return this;
-        }
+		/**
+		 *
+		 * @param {string} className
+		 * @return {$}
+		 */
+		toggleClass( className ) {
+			this.each( element => {
+				element.classList.toggle( className );
+			} );
+			return this;
+		}
 
-        /**
-         * Hide elements
-         */
-        hide() {
-            this.each(element => {
-                element.style.display = 'none';
-            });
-            return this;
-        }
+		/**
+		 * @param {string} className
+		 * @return {boolean}
+		 */
+		hasClass( className ) {
+			return this.elements[0].classList.contains( className );
+		}
 
-        /**
-         * Toggle elements visibility
-         */
-        toggle() {
-            this.each(element => {
-                element.style.display = (element.style.display === 'none') ? '' : 'none';
-            });
-            return this;
-        }
+		/**
+		 * Get closest element
+		 *
+		 * @param {string} selector
+		 * @return {$}
+		 */
+		closest( selector ) {
+			return new $( this.elements[0].closest( selector ) );
+		}
 
-        /**
-         * Set element attribute
-         * 
-         * @param {string} name 
-         * @param {string} value 
-         */
-        attr(name, value) {
-            this.each(element => {
-                if (!value) {
-                    return element.getAttribute(name);
-                }
-                element.setAttribute(name, value);
-            });
-            return this;
-        }
+		/**
+		 * Get parent element
+		 *
+		 * @return {$}
+		 */
+		parent() {
+			return new $( this.elements[0].parentElement );
+		}
 
-        /**
-         * Remove element attribute
-         * 
-         * @param {string} name 
-         */
-        removeAttr(name) {
-            this.each(element => element.removeAttribute(name));
-            return this;
-        }
+		/**
+		 * Get first element
+		 *
+		 * @return {$}
+		 */
+		first() {
+			return new $( this.elements[0] );
+		}
 
-        /**
-         * Update or get elements html content
-         * @param {string} html 
-         */
-        html(html) {
-            this.each( element => {
-                if (!html) {
-                    return element.innerHTML;
-                }
-                element.innerHTML = html;
-            });
-            return this;
-        }
+		/**
+		 * Get last element
+		 *
+		 * @return {$}
+		 */
+		last() {
+			return new $( this.elements[ this.elements.length -1 ] );
+		}
 
-        /**
-         * Update or get elements text content
-         * @param {string} html 
-         */
-        text(text) {
-            this.each( element => {
-                if (!text) {
-                    return element.innerText;
-                }
-                element.innerText = text;
-            });
-            return this;
-        }
+		/**
+		 * @param {number} index
+		 * @return {$}
+		 */
+		eq( index ) {
+			return new $( this.elements[ index ] );
+		}
 
-       /**
-        * Get or set data attribute
-        * @param {string} key 
-        * @param {string} value 
-        */
-        data(key, value) {
-            this.each( element => {
-                if (!value) {
-                    return element.dataset[key];
-                }
-                element.dataset[key] = value;
-            });
-            return this;
-        }
+		/**
+		 * Remove elements
+		 */
+		remove() {
+			this.each( element => {
+				element.parentNode.removeChild( element );
+			} );
+		}
+		/**
+		 * Show elements
+		 *
+		 * @return {$}
+		 */
+		show() {
+			this.each( element => {
+				element.style.display = '';
+			} );
+			return this;
+		}
 
-        /**
-         * Append node
-         * @param {string} node 
-         */
-        append(node) {
-            this.each(element => {
-                element.append(node);
-            });
-            return this;
-        }
+		/**
+		 * Hide elements
+		 *
+		 * @return {$}
+		 */
+		hide() {
+			this.each( element => {
+				element.style.display = 'none';
+			} );
+			return this;
+		}
 
-        /**
-         * Prepend node
-         * @param {string} node 
-         */
-        prepend(node) {
-            this.each(element => {
-                element.prepend(node);
-            });
-            return this;
-        }
-    }
+		/**
+		 * Toggle elements visibility
+		 *
+		 * @return {$}
+		 */
+		toggle() {
+			this.each( element => {
+				element.style.display = ( element.style.display === 'none' ) ? '' : 'none';
+			} );
+			return this;
+		}
 
-    // Instantiate
-    return selector => {
-        const instantiated = instances.find( instance => instance.selector === selector )
+		/**
+		 * Set element attribute
+		 *
+		 * @param {string} name
+		 * @param {string} [value]
+		 * @returns {string|$}
+		 */
+		attr( name, value ) {
+			if ( ! value ) {
+				return this.elements[0].getAttribute( name );
+			}
+			this.each( element => {
+				element.setAttribute( name, value );
+			} );
+			return this;
+		}
 
-        // Check if selector is already instantiated
-        if ( instantiated ) {
-            return instantiated.instance;
-        }
+		/**
+		 * Remove element attribute
+		 *
+		 * @param {string} name
+		 * @return {$}
+		 */
+		removeAttr( name ) {
+			this.each( element => element.removeAttribute( name ) );
+			return this;
+		}
 
-        const instance = new $(selector);
+		/**
+		 * Update or get elements html content
+		 * @param {string} [html]
+		 * @returns {string|$}
+		 */
+		html( html ) {
+			if ( ! html ) {
+				return this.elements[0].innerHTML;
+			}
+			this.each( element => {
+				element.innerHTML = html;
+			} );
+			return this;
+		}
 
-        instances.push({ selector, instance });
+		/**
+		 * Update or get elements text content
+		 * @param {string} [text]
+		 * @returns {string|$}
+		 */
+		text( text ) {
+			if ( ! text ) {
+				return this.elements[0].innerText;
+			}
+			this.each( element => {
+				element.innerText = text;
+			} );
+			return this;
+		}
 
-        return instance;
-    }
+		/**
+		 * Get or set data attribute
+		 * @param {string} key
+		 * @param {string} [value]
+		 * @returns {string|$}
+		 */
+		data( key, value ) {
+			if ( ! value ) {
+				return this.elements[0].dataset[ key ];
+			}
+			this.each( element => {
+				element.dataset[ key ] = value;
+			} );
+			return this;
+		}
 
-}());
+		/**
+		 * Append node
+		 * @param {(Node|string)} node
+		 * @return {$}
+		 */
+		append( node ) {
+			this.each( element => {
+				element.append( node );
+			} );
+			return this;
+		}
+
+		/**
+		 * Prepend node
+		 * @param {(Node|string)} node
+		 * @return {$}
+		 */
+		prepend( node ) {
+			this.each( element => {
+				element.prepend( node );
+			} );
+			return this;
+		}
+
+		/**
+		 * @param {Object.<string, string>} styles
+		 * @example {fontWeight: 'bold'}
+		 * @return {$}
+		 */
+		css( styles ) {
+			this.each( element => {
+				for ( const [property, value] of Object.entries( styles ) ) {
+					element.style[property] = value;
+				}
+			} );
+			return this;
+		}
+	}
+
+	// Instantiate
+	return selector => {
+		const instantiated = instances.find( instance => instance.selector === selector );
+
+		// Check if selector is already instantiated
+		if ( instantiated ) {
+			return instantiated.instance;
+		}
+
+		const instance = new $( selector );
+
+		instances.push( { selector, instance } );
+
+		return instance;
+	};
+
+}() );
